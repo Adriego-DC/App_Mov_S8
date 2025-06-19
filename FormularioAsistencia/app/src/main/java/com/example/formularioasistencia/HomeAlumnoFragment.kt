9f7e574c99d894b81dd8e0c36c41bf2e7c109f59
Diego Adrian Davila Carrillo <adriegodc@gmail.com>
2025-06-18 23:58:01 -0500

    Subida inicial del proyecto App_Mov_S8

---
+++ FormularioAsistencia/app/src/main/java/com/example/formularioasistencia/HomeAlumnoFragment.kt
@@ -0,0 +1,8 @@
+package com.example.formularioasistencia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class HomeAlumnoFragment : Fragment(R.layout.fragment_home_alumno)
